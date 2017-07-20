4451d426d7e55405d629277c5f642f31fa093414
ishaanSejwal <ishaan.sejwal@gmail.com>
2017-07-20 13:03:55 +0530

    spm implementation WIP

--- Package.swift
+++ Package.swift
@@ -4,10 +4,8 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftGit2",
-    targets: [
-        Target(name: "SwiftGit2")
-    ],
     dependencies: [
         .Package(url: "https://github.com/antitypical/Result.git", majorVersion: 3, minor: 0),
-    ]
+    ],
+    exclude: ["Sources/SwiftGit2.h", "Sources/SwiftGit2.m"]
 )
